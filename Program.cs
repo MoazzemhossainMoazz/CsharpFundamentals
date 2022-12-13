8e01101b48973ecb0012158952437318f050bd70
Moazz <moazzemh222@gmail.com>
2022-12-13 23:32:21 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpFundamentals
+{
+    public class Person
+    {
+        public string firstName;
+        public string lastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My Name is " + firstName + " " + lastName);
+            Console.ReadLine();
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Person male = new Person();
+
+                male.firstName = "John";
+                male.lastName = "Doe";
+
+                male.Introduce();
+            }
+        }
+    }
+}
